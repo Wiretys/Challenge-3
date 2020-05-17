016bbc8e65811b4dd12e4ce24d87352d32bf36f9
Wiretys <wiretys@gmail.com>
2020-05-17 17:47:46 -0400

    fixed string and while statement - moved from the test file

--- jstester.js
+++ jstester.js
@@ -1,7 +1,7 @@
-// pick password lenght prompt
+// pick password length prompt
 var pwlen = window.prompt("Please pick a password length between 8 to 128.");
 
-// verify lenght
+// verify length
 while (pwlen < 8 || pwlen > 128) {
 
     var pwlen = window.prompt("Incorrect: Please a number for a password length between 8 to 128.");
@@ -12,7 +12,7 @@ var lowercaseConfirm = window.confirm("Will this password require lowercase char
 
 if (lowercaseConfirm) {
     var lwerCase = "yes";
-    
+
 } else {
     var lwerCase = "no";
 }
@@ -22,7 +22,7 @@ var uppercaseConfirm = window.confirm("Will this password require uppercase char
 
 if (uppercaseConfirm) {
     var upprCase = "yes";
-    
+
 } else {
     var upprCase = "no";
 }
@@ -32,7 +32,7 @@ var numConfirm = window.confirm("Will this password require numbers?");
 
 if (numConfirm) {
     var numCase = "yes";
-    
+
 } else {
     var numCase = "no";
 }
@@ -40,67 +40,61 @@ if (numConfirm) {
 //special characters verification
 var specialConfirm = window.confirm("Will this password require special characters?");
 
-if (specialConfirm ) {
+if (specialConfirm) {
     var speclCase = "yes";
-    
+
 } else {
     var speclCase = "no";
 }
 
-//lowercase randomized
-if(lwerCase = "yes") {
-    var lwerCaseVar = (
-        Math.floor(Math.random() * 26));
-    var lowerstr = "abcdefghijklmnopqrstuvwxyz";
-    var lwerRand = lowerstr.charAt (lwerCaseVar, lwerCaseVar);
-} 
+var genpwLen = 0;
+//empty string
 
+var currentstr = "";
 
+while (pwlen > currentstr.length) {
 
-//uppercase randomized
-if(upprCase = "yes") {
-    var upprCaseVar = (
-        Math.floor(Math.random() * 26));
-    var upperstr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var upprRand = upperstr.charAt (upprCaseVar, upprCaseVar);
-} 
-
-
- 
-// function to generate a random numeric value
-if (numConfirm = "yes") {
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
+    //lowercase randomized
+    if (lwerCase = "yes") {
+        var lwerCaseVar = (
+            Math.floor(Math.random() * 26));
+        var lowerstr = "abcdefghijklmnopqrstuvwxyz";
+        var lwerRand = lowerstr.charAt(lwerCaseVar, lwerCaseVar);
     }
-      var numRand = (getRandomInt(9));
+
+
+
+    //uppercase randomized
+    if (upprCase = "yes") {
+        var upprCaseVar = (
+            Math.floor(Math.random() * 26));
+        var upperstr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var upprRand = upperstr.charAt(upprCaseVar, upprCaseVar);
+    }
+
+
+
+    // function to generate a random numeric value
+    if (numConfirm = "yes") {
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
+        }
+        var numRand = (getRandomInt(9));
+    }
+
+
+
+    // function to generate a random special character
+    if (speclCase = "yes") {
+        var speclCaseVar = (
+            Math.floor(Math.random() * 22));
+        var speclStr = ".[]{}()<>*+-=!?^$|";
+        var speclRand = speclStr.charAt(speclCaseVar, speclCaseVar);
+    }
+
+    currentstr = (lwerRand + upprRand + numRand + speclRand + currentstr);
+
+    if (currentstr.length > pwlen) {
+        break;
+    }
 }
-
-
-
-// function to generate a random special character
-
-if(speclCase = "yes") {
-    var speclCaseVar = (
-        Math.floor(Math.random() * 22));
-    var speclStr = ".[]{}()<>*+-=!?^$|";
-    var speclRand = speclStr.charAt (speclCaseVar,speclCaseVar);
-} 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-window.alert (lwerRand + upprRand + numRand + speclRand)
- 
-
-
-
-
--- script.js
+++ script.js
@@ -1,25 +1,112 @@
 // Assignment code here
 
-var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+// pick password length prompt
+var pwlen = window.prompt("Please pick a password length between 8 to 128.");
+
+// verify length
+while (pwlen < 8 || pwlen > 128) {
+
+    var pwlen = window.prompt("Incorrect: Please a number for a password length between 8 to 128.");
+}
+
+//lowercase verification
+var lowercaseConfirm = window.confirm("Will this password require lowercase characters?");
+
+if (lowercaseConfirm) {
+    var lwerCase = "yes";
+
+} else {
+    var lwerCase = "no";
+}
+
+//uppercase verification
+var uppercaseConfirm = window.confirm("Will this password require uppercase characters?");
+
+if (uppercaseConfirm) {
+    var upprCase = "yes";
+
+} else {
+    var upprCase = "no";
+}
+
+//numeric verification
+var numConfirm = window.confirm("Will this password require numbers?");
+
+if (numConfirm) {
+    var numCase = "yes";
+
+} else {
+    var numCase = "no";
+}
+
+//special characters verification
+var specialConfirm = window.confirm("Will this password require special characters?");
+
+if (specialConfirm) {
+    var speclCase = "yes";
+
+} else {
+    var speclCase = "no";
+}
+
+var genpwLen = 0;
+//empty string
+
+var currentstr = "";
+
+while (pwlen > currentstr.length) {
+
+    //lowercase randomized
+    if (lwerCase = "yes") {
+        var lwerCaseVar = (
+            Math.floor(Math.random() * 26));
+        var lowerstr = "abcdefghijklmnopqrstuvwxyz";
+        var lwerRand = lowerstr.charAt(lwerCaseVar, lwerCaseVar);
+    }
 
 
-var pwlength = window.prompt("Please pick a character length between 8 to 128.");
+
+    //uppercase randomized
+    if (upprCase = "yes") {
+        var upprCaseVar = (
+            Math.floor(Math.random() * 26));
+        var upperstr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var upprRand = upperstr.charAt(upprCaseVar, upprCaseVar);
+    }
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-var pwlenghtquestion = function() {
+
+    // function to generate a random numeric value
+    if (numConfirm = "yes") {
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
+        }
+        var numRand = (getRandomInt(9));
+    }
 
 
-  if (pwlength < 8 && pwlength > 128) {
-    pwlenghtquestion();
-  
-    var pwlength = window.prompt("Please pick a character length between 8 to 128.");
 
-  }
+    // function to generate a random special character
+    if (speclCase = "yes") {
+        var speclCaseVar = (
+            Math.floor(Math.random() * 22));
+        var speclStr = ".[]{}()<>*+-=!?^$|";
+        var speclRand = speclStr.charAt(speclCaseVar, speclCaseVar);
+    }
 
+    currentstr = (lwerRand + upprRand + numRand + speclRand + currentstr);
+
+    if (currentstr.length > pwlen) {
+        break;
+    }
+};
+
+var password = currentstr;
 
 // Write password to the #password input
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");

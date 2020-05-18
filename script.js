b3810027ba10b0fdb1ab39b56ecaf74082e1e807
Wiretys <wiretys@gmail.com>
2020-05-17 21:42:23 -0400

    removed test files - cleaned up code - fixed falsey and truthy issue - fixed undefined issue

--- script.js
+++ script.js
@@ -12,82 +12,50 @@ while (pwlen < 8 || pwlen > 128) {
 //lowercase verification
 var lowercaseConfirm = window.confirm("Will this password require lowercase characters?");
 
-if (lowercaseConfirm) {
-    var lwerCase = "yes";
-
-} else {
-    var lwerCase = "no";
-}
-
 //uppercase verification
 var uppercaseConfirm = window.confirm("Will this password require uppercase characters?");
-
-if (uppercaseConfirm) {
-    var upprCase = "yes";
-
-} else {
-    var upprCase = "no";
-}
 
 //numeric verification
 var numConfirm = window.confirm("Will this password require numbers?");
 
-if (numConfirm) {
-    var numCase = "yes";
-
-} else {
-    var numCase = "no";
-}
-
 //special characters verification
 var specialConfirm = window.confirm("Will this password require special characters?");
 
-if (specialConfirm) {
-    var speclCase = "yes";
-
-} else {
-    var speclCase = "no";
-}
-
-var genpwLen = 0;
-//empty string
-
+//empty strings to protect from undefined
 var currentstr = "";
+var lwerRand = "";
+var upprRand = "";
+var numRand = "";
+var speclRand = "";
 
 while (pwlen > currentstr.length) {
 
     //lowercase randomized
-    if (lwerCase = "yes") {
+    if (lowercaseConfirm === true) {
         var lwerCaseVar = (
             Math.floor(Math.random() * 26));
         var lowerstr = "abcdefghijklmnopqrstuvwxyz";
         var lwerRand = lowerstr.charAt(lwerCaseVar, lwerCaseVar);
     }
 
-
-
     //uppercase randomized
-    if (upprCase = "yes") {
+    if (uppercaseConfirm === true) {
         var upprCaseVar = (
             Math.floor(Math.random() * 26));
         var upperstr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         var upprRand = upperstr.charAt(upprCaseVar, upprCaseVar);
     }
 
-
-
     // function to generate a random numeric value
-    if (numConfirm = "yes") {
+    if (numConfirm === true) {
         function getRandomInt(max) {
             return Math.floor(Math.random() * Math.floor(max));
         }
         var numRand = (getRandomInt(9));
     }
 
-
-
     // function to generate a random special character
-    if (speclCase = "yes") {
+    if (specialConfirm === true) {
         var speclCaseVar = (
             Math.floor(Math.random() * 22));
         var speclStr = ".[]{}()<>*+-=!?^$|";
@@ -103,16 +71,14 @@ while (pwlen > currentstr.length) {
 
 var password = currentstr;
 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
-
-
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
